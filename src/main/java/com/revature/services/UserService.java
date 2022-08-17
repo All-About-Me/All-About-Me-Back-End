25e5ba5d392fb70e9aaac3bb116719a7051a3335
Mike Kight <102483977+Mdkight@users.noreply.github.com>
2022-08-17 11:15:48 -0500

    corrected port

--- src/main/java/com/revature/services/UserService.java
+++ src/main/java/com/revature/services/UserService.java
@@ -1,18 +1,27 @@
 package com.revature.services;
 
+import com.revature.models.Bookmark;
+import com.revature.models.Post;
 import com.revature.models.User;
+import com.revature.repositories.BookmarkRepository;
 import com.revature.repositories.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
-
-    public UserService(UserRepository userRepository) {
+    private final BookmarkRepository bookmarkRepository;
+    
+    @Autowired
+    public UserService(UserRepository userRepository, BookmarkRepository bookmarkRepository) {
         this.userRepository = userRepository;
+        this.bookmarkRepository =bookmarkRepository;
     }
 
     public Optional<User> findByCredentials(String email, String password) {
@@ -21,5 +30,19 @@ public class UserService {
 
     public User save(User user) {
         return userRepository.save(user);
+    }
+    
+    public List<Bookmark> retrieveBookmarks(User user){
+    	return bookmarkRepository.findByUserId(user);
+    }
+    
+    public Bookmark bookmarkPost(User user, Post post) {
+    	Bookmark bookmark= new Bookmark(post, user);
+    	return bookmarkRepository.save(bookmark);
+    }
+    
+    public void removeBookmark(Bookmark bookmark) {
+    	bookmarkRepository.delete(bookmark);
+    	//TODO add delete confirmation 
     }
 }

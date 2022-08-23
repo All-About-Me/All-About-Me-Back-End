0e978b98ceb9dceb1563326e9bf432c256fe484a
Austin313 <32691123+Austin313@users.noreply.github.com>
2022-08-23 15:05:59 -0500

    Update SocialMediaApplication.java

--- src/main/java/com/revature/SocialMediaApplication.java
+++ src/main/java/com/revature/SocialMediaApplication.java
@@ -2,8 +2,10 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = {"http://localhost:4200","http://aamfront-enddeploy.s3-website-us-east-1.amazonaws.com/"}, allowCredentials = "true")
 public class SocialMediaApplication {
 
 	public static void main(String[] args) {

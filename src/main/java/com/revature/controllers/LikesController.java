d984b3cb064f916e1e829da79babd83eb9d9652b
AngelaHsiao <angelaphsiao@gmail.com>
2022-08-26 07:17:39 -0700

    minor change for cors fix

--- src/main/java/com/revature/controllers/LikesController.java
+++ src/main/java/com/revature/controllers/LikesController.java
@@ -2,6 +2,7 @@ package com.revature.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import com.revature.services.LikesService;
 
 @RestController
 @RequestMapping("/liked")
+@CrossOrigin(origins = {"http://localhost:4200","http://aamfront-enddeploy.s3-website-us-east-1.amazonaws.com"}, allowCredentials = "true")
 public class LikesController {
 
 

f13a25bf4887264e2a561812e5a6948f792ecce4
Matson Mathew <matson375@revature.net>
2022-08-27 19:14:35 -0500

    MM0827_Refactored user repository JUnitTest

--- src/test/java/com/revature/repositories/UserRepositoryTest.java
+++ src/test/java/com/revature/repositories/UserRepositoryTest.java
@@ -3,7 +3,9 @@ package com.revature.repositories;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,18 +19,34 @@ class UserRepositoryTest {
 	@Autowired
 	UserRepository userReposiory;
 	
-	//JUnit test for save User operation
-	@DisplayName("User save successfull")
-	@Test
-	void givenUserObj_whenSave_thenReturnSavedUser() {
+	private User user;
+	
+	@BeforeEach
+	public void setup() {
 		
-		//given -precondition or setup
-		User user = User.builder()
+		 user = User.builder()
 				.id(2)
 				.firstName("Jasdhir")
 				.lastName("Singh")
+				.email("test01@gmail.com")
+				.password("password01")
 				.city("Mumbai")
 				.build();
+	
+	}
+	
+	//JUnit test for save User operation
+	@DisplayName("User save successfull")
+	@Test
+	public void givenUserObj_whenSave_thenReturnSavedUser() {
+		
+//		//given -precondition or setup
+//		User user = User.builder()
+//				.id(2)
+//				.firstName("Jasdhir")
+//				.lastName("Singh")
+//				.city("Mumbai")
+//				.build();
 		//when -action or the behaviour that we are going to test
 		User savedUser = userReposiory.save(user);
 		
@@ -40,15 +58,16 @@ class UserRepositoryTest {
 	
 	
 	//JUnit test for get all User operation
+	@DisplayName("Find all User successfull")
 	@Test
-	void givenUserList_whenFindAll_thenEmployeeList() {
+	void givenUserList_whenFindAll_thenUserList() {
 		//given - precondition or setup
-		User user1 = User.builder()
-				.id(1)
-				.firstName("Jasdhir")
-				.lastName("Singh")
-				.city("Mumbai")
-				.build();
+//		User user1 = User.builder()
+//				.id(1)
+//				.firstName("Jasdhir")
+//				.lastName("Singh")
+//				.city("Mumbai")
+//				.build();
 		
 		User user2 = User.builder()
 				.id(2)
@@ -57,7 +76,7 @@ class UserRepositoryTest {
 				.city("Mumbai")
 				.build();
 		
-		userReposiory.save(user1);
+		userReposiory.save(user);
 		userReposiory.save(user2);
 		
 		
@@ -69,5 +88,62 @@ class UserRepositoryTest {
 		assertThat((userList).size()).isEqualTo(2);
 				
 		}
+	
+	
+	
+	//JUnit test for get User by email and password
+	@DisplayName("Get User by Email and Password successfull")
+	@Test
+	void givenEmailAndPassword_whenFindByEmailAndPassword_thenUser()
+	{
+		//given - precondition or setup
+//		User user = User.builder()
+//				.id(2)
+//				.firstName("Jasdhir")
+//				.lastName("Singh")
+//				.email("test01@gmail.com")
+//				.password("password01")
+//				.city("Mumbai")
+//				.build();
+		
+		userReposiory.save(user);
+		
+		//when -action or the behaviour that we are going to test
+		Optional<User> userDB = userReposiory.findByEmailAndPassword(user.getEmail(),user.getPassword());
+		
+		//then - verify the object
+		assertThat(userDB).isNotNull();
+		}
+	
+	
+	
+	//JUnit test for update User Operation
+	@DisplayName("Update Password successfull")
+	@Test
+	void givenUserObject_whenUpdateUser_thenReturnUpdatedUser()
+	{
+		//given - precondition or setup
+//				User user = User.builder()
+//						.id(2)
+//						.firstName("Jasdhir")
+//						.lastName("Singh")
+//						.email("test01@gmail.com")
+//						.password("password01")
+//						.city("Mumbai")
+//						.build();
+				
+				userReposiory.save(user);
+		
+		//when -action or the behaviour that we are going to test
+		User savedUser = userReposiory.findById(user.getId()).get();
+		savedUser.setPassword("pass");
+		User updatedUser = userReposiory.save(savedUser);
+		
+		//then - verify the object
+		assertThat(updatedUser.getPassword()).isEqualTo("pass");
+				
+	}
+	
 
+	
 }

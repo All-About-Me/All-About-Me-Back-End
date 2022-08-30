208a9b76e4395b38574a4f13181a7fd5ad3fe42d
Mike Kight <102483977+Mdkight@users.noreply.github.com>
2022-08-30 09:43:27 -0500

    adjusted data.sql, testuser now has id of 1500

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,12 +11,12 @@ INSERT INTO posts (id, text, date, image_url, author_id) VALUES (
     'The classic',
     '2022-08-28 01:01:01',
     'https://i.imgur.com/fhgzVEt.jpeg',
-    1
+    1500
 ),
 (
     10001,
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     '2022-08-28 01:01:01',
     '',
-    1
+    1500
 ); 
